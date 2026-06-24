48a2d0f5126e97353172ce4b712d9f6cfd2d7b16
SuSuDear <18998441194@189.cn>
2026-06-24 21:38:16 +0800

    Auto enter new tabs from overview

--- browser/Reynard/Client/Interface/BrowserViewController+BrowserActions.swift
+++ browser/Reynard/Client/Interface/BrowserViewController+BrowserActions.swift
@@ -69,6 +69,7 @@ extension BrowserViewController {
                     mode: mode.tabMode
                 )
                 self.tabBar.setPendingExpansion(at: newTabIndex)
+                self.setTabOverviewVisible(false, animated: true)
             }
         } else {
             let newTabIndex = tabManager.createTab(selecting: true)
